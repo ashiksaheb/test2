6ff6cb504b72931423e7ea41233407cbcb616552
ashiksaheb <app.ashikurrahman@gmail.com>
2024-12-19 12:46:56 +0600

    add image

--- lib/main.dart
+++ lib/main.dart
@@ -105,6 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Image.asset('assets/images/image1.jpg'),
             const Text(
               'You have pushed the button this many times:',
             ),
